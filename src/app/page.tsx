731d64aa0a347c1208cf329ad31533791bdd6e2a
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-16 14:37:10 +0200

    Creating the nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="text-center flex justify-center items-center bg-red-700 underline w-full h-20 text-white">
+      Hello app!
+    </div>
+  );
+}
