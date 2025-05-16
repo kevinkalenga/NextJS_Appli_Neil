7629e83f3f0c30da7c6e3120ef762cdca98315db
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-16 17:24:16 +0200

    Creating additional routes

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,35 +1,35 @@
 import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react"
-import {GiMatchTip} from 'react-icons/gi'
+import { GiMatchTip } from 'react-icons/gi'
 import Link from "next/link"
 export default function TopNav() {
   return (
-    <Navbar 
-       maxWidth="xl" 
-       className="bg-gradient-to-r from-purple-400 to-purple-700" 
-       classNames={{
-         item: [
-            'text-xl',
-            'text-white',
-            'uppercase'
-         ]
-       }}
+    <Navbar
+      maxWidth="xl"
+      className="bg-gradient-to-r from-purple-400 to-purple-700"
+      classNames={{
+        item: [
+          'text-xl',
+          'text-white',
+          'uppercase'
+        ]
+      }}
     >
-        <NavbarBrand as={Link} href='/' >
-            <GiMatchTip size={40} className="text-gray-200" />
-            <div className="font-bold text-3xl flex">
-                <span className="text-gray-900">Next</span>
-                <span className="text-gray-200">Match</span>
-            </div>
-        </NavbarBrand>
-        <NavbarContent justify="center">
-            <NavbarItem as={Link} href='/members'>Matches</NavbarItem>
-            <NavbarItem as={Link} href='/lists'>Lists</NavbarItem>
-            <NavbarItem as={Link} href='/messages'>Messages</NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-             <Button variant="bordered" className="text-white">Login</Button>
-             <Button variant="bordered" className="text-white">Register</Button>
-        </NavbarContent>
+      <NavbarBrand as={Link} href='/' >
+        <GiMatchTip size={40} className="text-gray-200" />
+        <div className="font-bold text-3xl flex">
+          <span className="text-gray-900">Next</span>
+          <span className="text-gray-200">Match</span>
+        </div>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem as={Link} href='/members'>Matches</NavbarItem>
+        <NavbarItem as={Link} href='/lists'>Lists</NavbarItem>
+        <NavbarItem as={Link} href='/messages'>Messages</NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <Button as={Link} href="/auth/login" variant="bordered" className="text-white">Login</Button>
+        <Button as={Link} href="/auth/register" variant="bordered" className="text-white">Register</Button>
+      </NavbarContent>
     </Navbar>
   )
 }
