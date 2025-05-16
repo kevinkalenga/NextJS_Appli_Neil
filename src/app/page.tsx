caccf27a3087813454f09c31936e3f7d90bfceca
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-16 15:12:24 +0200

    What is in the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="text-center flex justify-center items-center bg-red-700 underline w-full h-20 text-white">
+    <div className="text-center text-2xl flex justify-center items-center bg-red-700 w-full p-4 text-white">
       Hello app!
     </div>
   );
